84425250dad37f2b257141d86ea4a1b445e45723
Josh Cummings <joshcummingsdesign@gmail.com>
2022-07-13 13:05:50 -0700

    Make current tada list unique

---
+++ database/migrations/2022_07_13_200155_make_current_tada_list_unique.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up() {
+    Schema::table('current_tada_lists', function (Blueprint $table) {
+      $table->unique('user_id');
+    });
+  }
+
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down() {
+    Schema::table('current_tada_lists', function (Blueprint $table) {
+      $table->dropUnique('user_id');
+    });
+  }
+};
